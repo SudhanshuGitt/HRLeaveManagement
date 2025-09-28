8954ff97150a8006dc3426e912561825613c8ee4
Sudhanshu Sharma <sudhanshus9530@gmail.com>
2025-09-28 15:04:48 +0530

    Added Libaray services

--- HRLeaveManagement.Api/Program.cs
+++ HRLeaveManagement.Api/Program.cs
@@ -1,11 +1,22 @@
+using HRLeaveManagement.Application;
+using HRLeaveManagement.Infrastructure;
+using HRLeaveManagement.Persistance;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigurePersistanceServices(builder.Configuration);
+builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+builder.Services.ConfigureApplicationServices();
+
 
 var app = builder.Build();
 
--- HRLeaveManagement.Application/Profiles/MappingProfile.cs
+++ HRLeaveManagement.Application/Profiles/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HRLeaveManagement.Application.DTO.LeaveAllocation;
 using HRLeaveManagement.Application.DTO.LeaveRequest;
+using HRLeaveManagement.Application.DTO.LeaveType;
 using HRLeaveManagement.Domain;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,21 @@ namespace HRLeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
-            CreateMap<LeaveRequest,LeaveRequestDto>().ReverseMap();
-            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
-            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>()
+                .ForMember(dest => dest.DateRequested, opt => opt.MapFrom(src => src.DateCreated))
+                .ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion LeaveRequest
+
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
         }
     }
 }
